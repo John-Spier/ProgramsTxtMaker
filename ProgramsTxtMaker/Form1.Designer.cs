0fa3e51c7119db546d295557f4bed32eeb8508c3
Fish <Fish@AAAAAPC>
2022-04-05 16:13:38 -0400

    Add project files.

---
+++ ProgramsTxtMaker/Form1.Designer.cs
@@ -0,0 +1,245 @@
+﻿
+namespace ProgramsTxtMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Prestige Elite Std", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.Color.Red;
+            this.textBox1.Location = new System.Drawing.Point(179, 409);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(319, 30);
+            this.textBox1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Lithos Pro Regular", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(14, 422);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(159, 22);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Disc Root Dir.";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Font = new System.Drawing.Font("Trajan Pro", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(506, 405);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 39);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Browse";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Font = new System.Drawing.Font("Charlemagne Std", 48F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(179, 307);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(482, 87);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "GO";
+            this.button2.UseMnemonic = false;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.ForeColor = System.Drawing.Color.Blue;
+            this.textBox2.Location = new System.Drawing.Point(14, 12);
+            this.textBox2.MaxLength = 1000000;
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(647, 291);
+            this.textBox2.TabIndex = 4;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.ForeColor = System.Drawing.Color.SpringGreen;
+            this.checkBox1.Location = new System.Drawing.Point(12, 375);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(133, 19);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Auto-Rename Files";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.ForeColor = System.Drawing.Color.OrangeRed;
+            this.checkBox2.Location = new System.Drawing.Point(12, 350);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(164, 19);
+            this.checkBox2.TabIndex = 6;
+            this.checkBox2.Text = "Auto-Rename Directories";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Orator Std", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericUpDown1.Location = new System.Drawing.Point(14, 317);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(41, 27);
+            this.numericUpDown1.TabIndex = 7;
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Orator Std", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericUpDown2.Location = new System.Drawing.Point(130, 317);
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(41, 27);
+            this.numericUpDown2.TabIndex = 8;
+            this.numericUpDown2.Value = new decimal(new int[] {
+            23,
+            0,
+            0,
+            0});
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 6.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label2.Location = new System.Drawing.Point(49, 306);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 11);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "<- Start FNChar End->";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.checkBox3.Font = new System.Drawing.Font("OCR A Std", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.checkBox3.ForeColor = System.Drawing.Color.Magenta;
+            this.checkBox3.Location = new System.Drawing.Point(12, 400);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(112, 16);
+            this.checkBox3.TabIndex = 10;
+            this.checkBox3.Text = "Divide Dirs";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("MaplePi", 8.249999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.numericUpDown3.Location = new System.Drawing.Point(130, 397);
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(41, 22);
+            this.numericUpDown3.TabIndex = 11;
+            this.numericUpDown3.Value = new decimal(new int[] {
+            30,
+            0,
+            0,
+            0});
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(673, 450);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Brush Script Std", 9.749999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "PS1 Automatic EXE Menu Maker";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.DoubleClick += new System.EventHandler(this.Form1_DoubleClick);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+    }
+}
+
---
+++ ProgramsTxtMaker/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Windows.Forms;
+
+namespace ProgramsTxtMaker
+{
+    public partial class Form1 : Form
+    {
+
+
+        //[DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        //private static extern int GetShortPathName(String pathName, StringBuilder shortName, int cbShortName);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (folderBrowserDialog1.ShowDialog()==DialogResult.OK)
+            {
+                textBox1.Text = folderBrowserDialog1.SelectedPath;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //textBox2.Clear();
+            textBox2.Text = "START\r\n";
+            string tbone = textBox1.Text;
+            tbone = tbone.Trim();
+            tbone = tbone.TrimEnd('\\');
+            int sn = 1;
+            int dn = 0;
+            string oldfn;
+            string cdromdir;
+            string g;
+            string progfile = tbone + Path.DirectorySeparatorChar + "PROGRAMS.TXT";
+            if (checkBox3.Checked) { checkBox1.Checked = true; }
+            if (File.Exists(progfile))
+            {
+                //File.Move(progfile, tbone + Path.DirectorySeparatorChar + sn.ToString() + ".BAK");
+                
+            }
+            StreamWriter writer = File.CreateText(progfile);
+            writer.WriteLine("START");
+            foreach (string d in Directory.GetDirectories(tbone, "*", SearchOption.TopDirectoryOnly)) //can't rename inner directories
+            {
+                //textBox2.Text += d + "\r\n";
+                if (checkBox2.Checked)
+                {
+                    //MessageBox.Show(Path.GetDirectoryName(d) + "\\" + sn.ToString());
+                    try
+                    {
+                        Directory.Move(Path.GetFullPath(d), Path.GetDirectoryName(d) + "\\" + sn.ToString());
+                    } 
+                    catch (Exception dx)
+                    {
+                        this.Text = "Directory Error: " + dx.Message;
+                    }
+                    sn++;
+                }
+
+            }
+            foreach (string f in Directory.GetFiles(tbone, @"*.exe", SearchOption.AllDirectories))
+            {
+                try
+                {
+                    oldfn = Path.GetFileNameWithoutExtension(f);
+                    if (checkBox3.Checked)
+                    {
+                        if (sn % ((int)numericUpDown3.Value)==0)
+                        {
+                            dn++;
+                        }
+
+                        string h = Path.GetDirectoryName(f) + Path.DirectorySeparatorChar + dn.ToString();
+                        if (!Directory.Exists(h))
+                        {
+                            Directory.CreateDirectory(h);
+                        }
+                        
+                        sn++;
+                        g = h + Path.DirectorySeparatorChar + sn.ToString() + ".EXE";
+                        //MessageBox.Show(g);
+                        File.Move(Path.GetFullPath(f), g);
+
+                    }
+                    else if (checkBox1.Checked)
+                    {
+                        g = Path.GetDirectoryName(f) + Path.DirectorySeparatorChar + sn.ToString() + ".EXE";
+                        sn++;
+                        //MessageBox.Show(g);
+                        File.Move(Path.GetFullPath(f), g);
+                    } 
+                    
+                    else
+                    {
+                        g = Path.GetFullPath(f);
+                    }
+                    
+                    cdromdir = g.Substring(Path.GetFullPath(tbone).Length);
+                    //MessageBox.Show(cdromdir);
+                    textBox2.Text += '"' + oldfn.Substring(Math.Min((int)numericUpDown1.Value, oldfn.Length), Math.Min(((int)numericUpDown2.Value) - (int)numericUpDown1.Value, oldfn.Length - (int)numericUpDown1.Value)) + "\"cdrom:" + cdromdir + ";1\"\r\n";
+                    writer.WriteLine('"' + oldfn.Substring(Math.Min((int)numericUpDown1.Value, oldfn.Length), Math.Min(((int)numericUpDown2.Value) - (int)numericUpDown1.Value, oldfn.Length - (int)numericUpDown1.Value)) + "\"cdrom:" + cdromdir + ";1\"");
+                }
+                catch (Exception fx)
+                {
+                    this.Text = "File Error: " + fx.Message;
+                }
+            }
+            textBox2.Text += "\"END\"";
+            writer.WriteLine("\"END\"");
+            writer.Flush();
+            writer.Close();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImageLayout = ImageLayout.Center;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_DoubleClick(object sender, EventArgs e)
+        {
+            //MessageBox.Show("abc");
+            textBox1.Visible = !textBox1.Visible;
+            textBox2.Visible = !textBox2.Visible;
+            button1.Visible = !button1.Visible;
+            button2.Visible = !button2.Visible;
+            label1.Visible = !label1.Visible;
+            label2.Visible = !label2.Visible;
+            checkBox1.Visible = !checkBox1.Visible;
+            checkBox2.Visible = !checkBox2.Visible;
+            numericUpDown1.Visible = !numericUpDown1.Visible;
+            numericUpDown2.Visible = !numericUpDown2.Visible;
+            checkBox3.Visible = !checkBox3.Visible;
+            numericUpDown3.Visible = !numericUpDown3.Visible;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+            this.BackgroundImageLayout = ImageLayout.Stretch;
+        }
+
+
+    }
+}
